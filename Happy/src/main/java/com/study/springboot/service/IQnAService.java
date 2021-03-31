e08009d8f59c6bd4bf01dd22fd709c6bc17a4b6c
01072 <01072@LAPTOP-ETM5S35P>
2021-03-31 15:19:15 +0900

    수정

--- Happy/src/main/java/com/study/springboot/service/IQnAService.java
+++ Happy/src/main/java/com/study/springboot/service/IQnAService.java
@@ -2,10 +2,16 @@ package com.study.springboot.service;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.study.springboot.dto.NoticeDto;
 import com.study.springboot.dto.QnADto;
 
 public interface IQnAService {
 
 	public int write(String Id, String qbName, String qbTitle, String qbContent);
+	public int update(HttpServletRequest req);
 	public ArrayList<QnADto> list();
+	public QnADto contentview(String bid_str);
+	public int delete(String bid);
 }
