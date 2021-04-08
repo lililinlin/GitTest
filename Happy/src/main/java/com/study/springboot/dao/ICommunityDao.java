3d224d4ee26b7322d708e0c5dbb4e3475a7045ef
01072 <01072@LAPTOP-ETM5S35P>
2021-04-08 15:55:05 +0900

    수정

--- Happy/src/main/java/com/study/springboot/dao/ICommunityDao.java
+++ Happy/src/main/java/com/study/springboot/dao/ICommunityDao.java
@@ -1,5 +1,10 @@
 package com.study.springboot.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface ICommunityDao {
 
+	public int writedao(String Id,String cbName,String cbTitle,String cbContent);
+	
 }
--- Happy/src/main/java/com/study/springboot/service/CommunityService.java
+++ Happy/src/main/java/com/study/springboot/service/CommunityService.java
@@ -1,5 +1,22 @@
 package com.study.springboot.service;
 
-public class CommunityService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.study.springboot.dao.ICommunityDao;
+import com.study.springboot.dto.CommunityDto;
+
+@Service
+public class CommunityService implements ICommunityService{
+
+	@Autowired
+	ICommunityDao comdao;
+	@Autowired
+	CommunityDto comdto;
+	
+	@Override
+	public int write(String Id,String cbName,String cbTitle,String cbContent) {
+		int nResult = comdao.writedao(Id,cbName,cbTitle,cbContent);
+		return nResult;
+	}
 }
